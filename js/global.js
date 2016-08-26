cf8cde5e719b4ca58c9c6efa3d48e4163ca7d34d
troy <914053923@qq.com>
2016-08-26 11:37:46 +0800

    add global cnzz

---
+++ js/global.js
@@ -0,0 +1,9 @@
+(function () {
+    // for cnzz
+    var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
+    document.write(decodeURI("%3Cspan id='cnzz_stat_icon_1260076495'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php?id=3D1260076495' type='text/javascript'%3E%3C/script%3E"));
+    // hide cnzz
+    var style=document.createElement('style');
+    style.innerText='a[title="站长统计"]{display:none};';
+    document.head.appendChild(style);
+})();
