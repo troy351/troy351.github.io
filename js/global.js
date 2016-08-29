dfef7bc667fd5e32e808f71e00024091c4cfeb17
troy <914053923@qq.com>
2016-08-29 23:58:52 +0800

    change cnzz load method

--- js/global.js
+++ js/global.js
@@ -1,8 +1,7 @@
 (function () {
     // for cnzz
-    var script=document.createElement('script');
-    script.src='https://s4.cnzz.com/z_stat.php?id=1260076495&web_id=1260076495';
-    document.body.appendChild(script);
+    var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    document.write(unescape("%3Cspan id='cnzz_stat_icon_1260076495'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1260076495' type='text/javascript'%3E%3C/script%3E"));
     // hide cnzz
     var style=document.createElement('style');
     style.innerText='a[title="站长统计"]{display:none};';
